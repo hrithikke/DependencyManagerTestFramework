24ccdba03c61b08268118348d29daed951b3b89c
Hrithik Kesharwani <hrithikk@cybage.com>
2023-02-27 14:25:33 +0530

    Added Framework folder

---
+++ Frameworks/DependencyManagerTest.xcframework/ios-arm64/DependencyManagerTest.framework/Headers/DependencyManagerTest.h
@@ -0,0 +1,18 @@
+//
+//  DependencyManagerTest.h
+//  DependencyManagerTest
+//
+//  Created by Hrithik Kesharwani on 27/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependencyManagerTest.
+FOUNDATION_EXPORT double DependencyManagerTestVersionNumber;
+
+//! Project version string for DependencyManagerTest.
+FOUNDATION_EXPORT const unsigned char DependencyManagerTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependencyManagerTest/PublicHeader.h>
+
+
